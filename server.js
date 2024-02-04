d159ff01af16fce414338ffe368e837df40e2805
yash kshatriya <yashkshatriya.67@gmail.com>
2024-02-04 15:09:39 +0530

    commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(router);
 
 
 
-app.use(express.static(path.join(__dirname,"../client/build")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname,"./client/build/index.html"));
